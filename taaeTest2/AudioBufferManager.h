3d547c97e4e3a1338c56b49f5585f5d1bc2421f5
Sander Valstar <sanderman123@hotmail.com>
2015-01-31 13:30:37 +0800

    Fixed random crashes (no more malloc!!) In decode audio buffer list function already send data to the audio player instead of looping through the buffer lists again later.

---
+++ taaeTest2/AudioBufferManager.h
@@ -0,0 +1,13 @@
+//
+//  AudioBufferManager.h
+//  taaeTest2
+//
+//  Created by Sander Valstar on 1/31/15.
+//  Copyright (c) 2015 Sander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AudioBufferManager : NSObject
+
+@end
